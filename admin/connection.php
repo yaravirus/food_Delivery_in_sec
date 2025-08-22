47f459c9f2766f67adb11786cc7462bb32918183
durgesh <streamvolt000gmail.com>
2025-08-23 00:48:11 +0530

    new connection

--- admin/connection.php
+++ admin/connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv("DB_HOST") ?: "mysql-db";  // mysql-db is the service name on Render
+$host = getenv("DB_HOST") ?: "mysql-db";  // mysql-db = Render MySQL service name
 $db   = getenv("DB_NAME") ?: "food_delivery";
 $user = getenv("DB_USER") ?: "fooduser";
-$pass = getenv("DB_PASS") ?: "foodpass";
+$pass = getenv("DB_PASS") ?: "foodpass123";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
